19032ba7dc7c68b1e2bf4e98b1cc0d76b24b7b11
ywu421 <ucbv076@ucl.ac.uk>
2024-12-02 00:53:23 +0000

    remove unwanted message

--- periodicCall.php
+++ periodicCall.php
@@ -2,14 +2,13 @@
     echo '
         <div>
             <script>
-                console.log("ss")
                 fetch(window.location.origin+"/inbox.php?check_auctions=true").then(async r => console.log("cao"))
                 setInterval(function() {
                     $.ajax({    
                         url: "inbox.php?check_auctions=true",
                         type: "GET",
                         success: function(data) {
-                            console.log("Auction check complete:", data);
+                            // console.log("Auction check complete:", data);
                         }
                     });
                 }, 10000);
